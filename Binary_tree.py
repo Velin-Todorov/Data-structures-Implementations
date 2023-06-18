32decd040613d6c548c2ab0d7586255b5446d5be
Velin-Todorov <velintodorov@yahoo.com>
2023-06-18 22:05:33 +0200

    implemented structure

---
+++ Binary_tree.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, key, right=None, left=None) -> None:
+        self.key = key
+        self.right = right
+        self.left = left
+    
+
+
+class Tree:
+    def __init__(self, root: Node):
+        self.root = root
+    
+
+    #traversal operations
+    def inOrder(self):
+        pass
+
+    def preOrder(self):
+        pass
+
+    def postOrder(self):
+        pass
+
+    def levelOrder(self):
+        pass
+
+    #insertions
+    def insert(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+    def search(self):
+        pass
+
+    def return_height(self):
+        pass
+
+
+    def return_size(self):
+        pass
+
+    
+    def level_of_node(self):
+        pass
+
+    
